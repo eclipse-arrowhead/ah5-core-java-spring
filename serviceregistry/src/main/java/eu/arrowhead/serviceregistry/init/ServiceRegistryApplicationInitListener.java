ad767764ea01c21839394ffd90d669101532a47c
Rajmund Bocsi <rbocsi@aitia.ai>
2024-07-02 14:42:24 +0200

    application initialization (skeleton class)

---
+++ serviceregistry/src/main/java/eu/arrowhead/serviceregistry/init/ServiceRegistryApplicationInitListener.java
@@ -0,0 +1,10 @@
+package eu.arrowhead.serviceregistry.init;
+
+import org.springframework.stereotype.Component;
+
+import eu.arrowhead.common.init.ApplicationInitListener;
+
+@Component
+public class ServiceRegistryApplicationInitListener extends ApplicationInitListener {
+
+}
