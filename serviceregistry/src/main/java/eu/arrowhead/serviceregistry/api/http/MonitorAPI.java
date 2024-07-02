7e5f4ccfcf5c5495c5c82a1abfdca1a97ec913ab
borditamas <borditamas@gmail.com>
2024-07-02 10:06:52 +0200

    Swagger integration

--- serviceregistry/src/main/java/eu/arrowhead/serviceregistry/api/http/MonitorAPI.java
+++ serviceregistry/src/main/java/eu/arrowhead/serviceregistry/api/http/MonitorAPI.java
@@ -1,10 +1,18 @@
 package eu.arrowhead.serviceregistry.api.http;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import eu.arrowhead.common.Constants;
+import eu.arrowhead.dto.ErrorMessageDTO;
 import eu.arrowhead.serviceregistry.ServiceRegistryConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping(ServiceRegistryConstants.HTTP_API_MONITOR_PATH)
@@ -14,6 +22,14 @@ public class MonitorAPI {
 	// methods
 	
 	//-------------------------------------------------------------------------------------------------
+	@Operation(summary = "Returns an echo message with the purpose of testing the service availability")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = Constants.HTTP_STATUS_OK, description = Constants.SWAGGER_HTTP_200_MESSAGE, content = { @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, schema = @Schema(implementation = String.class)) }),
+		@ApiResponse(responseCode = Constants.HTTP_STATUS_FORBIDDEN, description = Constants.SWAGGER_HTTP_403_MESSAGE,
+					 content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessageDTO.class)) }),
+		@ApiResponse(responseCode = Constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, description = Constants.SWAGGER_HTTP_500_MESSAGE,
+					 content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorMessageDTO.class)) })
+	})
 	@GetMapping(path = "/echo")
 	public String echoService() {
 		return "Got it!";
