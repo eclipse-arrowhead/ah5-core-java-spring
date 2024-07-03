383e727e9bed0f98bb8b0f830bb4a05216c56307
Rajmund Bocsi <rbocsi@aitia.ai>
2024-07-03 15:40:27 +0200

    service definition representation

---
+++ serviceregistry/src/main/java/eu/arrowhead/serviceregistry/jpa/entity/ServiceDefinition.java
@@ -0,0 +1,47 @@
+package eu.arrowhead.serviceregistry.jpa.entity;
+
+import eu.arrowhead.common.jpa.ArrowheadEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ServiceDefinition extends ArrowheadEntity {
+
+	//=================================================================================================
+	// members
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, unique = true, length = 63)
+	private String serviceDefinition; 
+
+	//=================================================================================================
+	// methods
+
+	//-------------------------------------------------------------------------------------------------
+	public ServiceDefinition() {}
+
+	//-------------------------------------------------------------------------------------------------
+	public ServiceDefinition(final String serviceDefinition) {
+		this.serviceDefinition = serviceDefinition;
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	public long getId() { return id; }
+	public String getServiceDefinition() { return serviceDefinition; }
+
+	//-------------------------------------------------------------------------------------------------
+	public void setId(final long id) { this.id = id; }
+	public void setServiceDefinition(final String serviceDefinition) { this.serviceDefinition = serviceDefinition; }
+
+	//-------------------------------------------------------------------------------------------------
+	@Override
+	public String toString() {
+		return "ServiceDefinition [id = " + id + ", serviceDefinition = " + serviceDefinition + "]";
+	}
+}
