a7a15d28caf1f3a995746c371eb3eb2ac80becce
borditamas <borditamas@gmail.com>
2024-10-18 16:00:56 +0200

    mqtt api (not finished)

--- serviceregistry/src/main/java/eu/arrowhead/serviceregistry/api/mqtt/SystemDiscoveryMqttHandler.java
+++ serviceregistry/src/main/java/eu/arrowhead/serviceregistry/api/mqtt/SystemDiscoveryMqttHandler.java
@@ -1,23 +1,33 @@
 package eu.arrowhead.serviceregistry.api.mqtt;
 
 import java.security.InvalidParameterException;
+import java.util.Map.Entry;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import eu.arrowhead.common.Constants;
 import eu.arrowhead.common.exception.ArrowheadException;
+import eu.arrowhead.common.mqtt.MqttStatus;
 import eu.arrowhead.common.mqtt.handler.MqttTopicHandler;
 import eu.arrowhead.common.mqtt.model.MqttRequestModel;
+import eu.arrowhead.dto.SystemRequestDTO;
+import eu.arrowhead.dto.SystemResponseDTO;
 import eu.arrowhead.serviceregistry.ServiceRegistryConstants;
+import eu.arrowhead.serviceregistry.service.SystemDiscoveryService;
 
 @Service
 public class SystemDiscoveryMqttHandler extends MqttTopicHandler {
 
 	//=================================================================================================
 	// members
+
+	@Autowired
+	private SystemDiscoveryService sdService;
 
 	private final Logger logger = LogManager.getLogger(getClass());
 
@@ -32,14 +42,19 @@ public class SystemDiscoveryMqttHandler extends MqttTopicHandler {
 
 	//-------------------------------------------------------------------------------------------------
 	@Override
-	public void handle(final MqttRequestModel request)  throws ArrowheadException {
+	public void handle(final MqttRequestModel request) throws ArrowheadException {
 		logger.debug("SystemDiscoveryMqttHandler.handle started");
 		Assert.isTrue(request.getRequestTopic().equals(topic()), "MQTT topic-handler mismatch");
 
+		MqttStatus responseStatus = MqttStatus.OK;
+		Object responsePayload = null;
+
 		switch (request.getOperation()) {
 		case Constants.SERVICE_OP_REGISTER:
-			// TODO
-			System.out.println("Do operation" + request.getOperation());
+			final SystemRequestDTO dto = readPayload(request.getPayload(), SystemRequestDTO.class);
+			final Pair<SystemResponseDTO, MqttStatus> result = register(dto);
+			responseStatus = result.getRight();
+			responsePayload = result.getLeft();
 			break;
 
 		case Constants.SERVICE_OP_LOOKUP:
@@ -47,11 +62,33 @@ public class SystemDiscoveryMqttHandler extends MqttTopicHandler {
 			break;
 
 		case Constants.SERVICE_OP_REVOKE:
-			System.out.println("Do operation " + request.getOperation());
+			final String name = readPayload(request.getPayload(), String.class);
+			responseStatus = revoke(name);
 			break;
 
 		default:
 			throw new InvalidParameterException("Unknown operation: " + request.getOperation());
 		}
+
+		successResponse(request, responseStatus, responsePayload);
+	}
+
+	//=================================================================================================
+	// assistant methods
+
+	//-------------------------------------------------------------------------------------------------
+	private Pair<SystemResponseDTO, MqttStatus> register(final SystemRequestDTO dto) {
+		logger.debug("SystemDiscoveryMqttHandler.register started");
+
+		final Entry<SystemResponseDTO, Boolean> result = sdService.registerSystem(dto, topic());
+		return Pair.of(result.getKey(), result.getValue() ? MqttStatus.CREATED : MqttStatus.OK);
+	}
+
+	//-------------------------------------------------------------------------------------------------
+	private MqttStatus revoke(final String systemName) {
+		logger.debug("SystemDiscoveryMqttHandler.revoke started");
+
+		boolean result = sdService.revokeSystem(systemName, topic());
+		return result ? MqttStatus.OK : MqttStatus.NO_CONTENT;
 	}
 }
