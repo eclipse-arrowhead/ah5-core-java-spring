efd01b50de6ffdf74854751f911bb0de77b9a0ac
Katinka666 <jako.katinka@gmail.com>
2024-12-02 13:14:01 +0100

    service normalization tests

--- serviceregistry/src/test/java/eu/arrowhead/serviceregistry/service/normalization/DeviceDiscoveryNormalizationTest.java
+++ serviceregistry/src/test/java/eu/arrowhead/serviceregistry/service/normalization/DeviceDiscoveryNormalizationTest.java
@@ -1,7 +1,6 @@
 package eu.arrowhead.serviceregistry.service.normalization;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -13,7 +12,6 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import eu.arrowhead.common.Utilities;
 import eu.arrowhead.dto.AddressDTO;
 import eu.arrowhead.dto.DeviceLookupRequestDTO;
 import eu.arrowhead.dto.DeviceRequestDTO;
@@ -46,12 +44,15 @@ public class DeviceDiscoveryNormalizationTest {
 		assertThrows(java.lang.IllegalArgumentException.class, () -> {normalizator.normalizeDeviceRequestDTO(new DeviceRequestDTO("", null, null));});
 		
 		// normalize dto
-		DeviceRequestDTO toNormalize = new DeviceRequestDTO(
+		final DeviceRequestDTO toNormalize = new DeviceRequestDTO(
+				// name
 				"\n \tdevice-NAME-1\r \n", 
+				// metadata
 				Map.of("key1", "value1", "key2", "value2"), 
+				// addresses
 				Arrays.asList(" 1.DEVICE.COM\n", " 1A-2B-3C-4D-5E-6F\n"));
 		
-		NormalizedDeviceRequestDTO normalized = normalizator.normalizeDeviceRequestDTO(toNormalize);
+		final NormalizedDeviceRequestDTO normalized = normalizator.normalizeDeviceRequestDTO(toNormalize);
 		assertAll("normalize DeviceRequestDTO",
 				// name
 				() -> assertEquals("device-name-1", normalized.name()), 
@@ -70,16 +71,20 @@ public class DeviceDiscoveryNormalizationTest {
 		assertThrows(java.lang.IllegalArgumentException.class, () -> {normalizator.normalizeDeviceLookupRequestDTO(null);});
 		
 		// normalize dto
-		List<MetadataRequirementDTO> metadataRequirements = new ArrayList<>(1);
-		metadataRequirements.add((MetadataRequirementDTO) new MetadataRequirementDTO().put("key.subkey", Map.of("op", "LESS_THAN", "value", 10)));
+		final List<MetadataRequirementDTO> metadataRequirements = new ArrayList<>(1);
+		metadataRequirements.add((MetadataRequirementDTO) new MetadataRequirementDTO().put("key1", Map.of("op", "EQUALS", "value", "value1")));
 		
-		DeviceLookupRequestDTO toNormalize = new DeviceLookupRequestDTO(
+		final DeviceLookupRequestDTO toNormalize = new DeviceLookupRequestDTO(
+				// names
 				List.of("\n \tdevice-NAME-1\r \n", "\n \tdevice-NAME-2\r \n"), 
+				// addresses
 				Arrays.asList(" 1.DEVICE.COM\n", " 2.DEVICE.ORG\n"), 
+				// address type
 				"hostname \n",
+				// metadata requirements
 				metadataRequirements);
 		
-		DeviceLookupRequestDTO normalized = normalizator.normalizeDeviceLookupRequestDTO(toNormalize);
+		final DeviceLookupRequestDTO normalized = normalizator.normalizeDeviceLookupRequestDTO(toNormalize);
 		assertAll("normalize DeviceLookupRequestDTO",
 				// names
 				() -> assertEquals(List.of("device-name-1", "device-name-2"), normalized.deviceNames()),
@@ -96,11 +101,13 @@ public class DeviceDiscoveryNormalizationTest {
 	@Test
 	public void normalizeDeviceNameTest() {
 		// name is null
-		assertThrows(java.lang.IllegalArgumentException.class, () -> {normalizator.normalizeDeviceLookupRequestDTO(null);});
+		assertThrows(java.lang.IllegalArgumentException.class, () -> {normalizator.normalizeDeviceName(null);});
+		
+		// name is empty
+		assertThrows(java.lang.IllegalArgumentException.class, () -> { normalizator.normalizeDeviceName(""); });
 		
 		// normalize name
 		assertEquals("device-name-1", normalizator.normalizeDeviceName("\n \tdevice-NAME-1\r \n"));
-		assertThrows(java.lang.IllegalArgumentException.class, () -> { normalizator.normalizeDeviceName(""); });
 	}
 
 }
---
+++ serviceregistry/src/test/java/eu/arrowhead/serviceregistry/service/normalization/SystemDiscoveryNormalizationTest.java
@@ -0,0 +1,121 @@
+package eu.arrowhead.serviceregistry.service.normalization;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import eu.arrowhead.dto.AddressDTO;
+import eu.arrowhead.dto.MetadataRequirementDTO;
+import eu.arrowhead.dto.SystemLookupRequestDTO;
+import eu.arrowhead.dto.SystemRequestDTO;
+import eu.arrowhead.serviceregistry.service.dto.NormalizedSystemRequestDTO;
+
+@SpringBootTest
+public class SystemDiscoveryNormalizationTest {
+	
+	//=================================================================================================
+	// members
+	
+	@Autowired
+	private SystemDiscoveryNormalization normalizator;
+	
+	//=================================================================================================
+	// methods
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void normalizeSystemRequestDTOTest() {
+		// dto is null
+		assertThrows(java.lang.IllegalArgumentException.class, () -> {normalizator.normalizeSystemRequestDTO(null);});
+		
+		// normalize dto
+		SystemRequestDTO toNormalize = new SystemRequestDTO(
+				// name
+				" \tsysTEM-namE \n ",
+				// metadata
+				Map.of("key", "value"),
+				// version
+				"1.3\r\r",
+				// addresses
+				List.of("2001:db8:85a3::8a2e:0370:7334\r\n", " 192.168.1.1\n \t"),
+				// device name
+				"\ndevice-nAME\n");
+		
+		NormalizedSystemRequestDTO normalized = normalizator.normalizeSystemRequestDTO(toNormalize);
+		
+		assertAll("normalize SystemRequestDTO",
+				// name
+				() -> assertEquals("system-name", normalized.name()),
+				// metadata (should not change)
+				() -> assertEquals(Map.of("key", "value"), normalized.metadata()),
+				// version
+				() -> assertEquals("1.3.0", normalized.version()),
+				// addresses
+				() -> assertEquals(List.of(new AddressDTO("IPV6", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"), new AddressDTO("IPV4", "192.168.1.1")), normalized.addresses()),
+				// device name
+				() -> assertEquals("device-name", normalized.deviceName()));
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void normalizeSystemLookupRequestDTO() {
+		// dto is null
+		assertDoesNotThrow(() -> normalizator.normalizeSystemLookupRequestDTO(null));
+		
+		// normalize dto
+		final List<MetadataRequirementDTO> metadataRequirements = new ArrayList<>(1);
+		metadataRequirements.add((MetadataRequirementDTO) new MetadataRequirementDTO().put("key1", Map.of("op", "EQUALS", "value", "value1")));
+	
+		SystemLookupRequestDTO toNormalize = new SystemLookupRequestDTO(
+				// names
+				List.of(" system-NAME-1\r\n", " system-NAME-2\r\n"), 
+				// addresses
+				List.of("2001:db8:85a3::8a2e:370:7334\r\n", "    2001:db8:85a3::8a2e:370:1\r\n"), 
+				// address type
+				" ipv6\r\n", 
+				// metadata requirements
+				metadataRequirements, 
+				// versions
+				List.of("", " 1 ", "1.1\r\n"), 
+				// device names
+				List.of(" device-NAME-1\r\n", " device-NAME-2\r\n"));
+		
+		SystemLookupRequestDTO normalized = normalizator.normalizeSystemLookupRequestDTO(toNormalize);
+		
+		assertAll("normalize SystemLookupRequestDTO",
+				// names
+				() -> assertEquals(List.of("system-name-1", "system-name-2"), normalized.systemNames()),
+				// addresses
+				() -> assertEquals(List.of("2001:0db8:85a3:0000:0000:8a2e:0370:7334", "2001:0db8:85a3:0000:0000:8a2e:0370:0001"), normalized.addresses()),
+				// address type
+				() -> assertEquals("IPV6", normalized.addressType()),
+				// metadata requirements (should not change)
+				() -> assertEquals(metadataRequirements, normalized.metadataRequirementList()),
+				// versions
+				() -> assertEquals(List.of("1.0.0", "1.0.0", "1.1.0"), normalized.versions()),
+				// device names
+				() -> assertEquals(List.of("device-name-1", "device-name-2"), normalized.deviceNames()));
+	}
+	
+	//-------------------------------------------------------------------------------------------------
+	@Test
+	public void normalizeRevokeSystemNameTest() {
+		// name is null
+		assertThrows(java.lang.IllegalArgumentException.class, () -> {normalizator.normalizeRevokeSystemName(null);});
+		
+		// name is empty
+		assertThrows(java.lang.IllegalArgumentException.class, () -> { normalizator.normalizeRevokeSystemName("");});
+		
+		// normalize name
+		assertEquals("system-name-1", normalizator.normalizeRevokeSystemName("\n \tsystem-NAME-1\r \n"));
+	}
+}
