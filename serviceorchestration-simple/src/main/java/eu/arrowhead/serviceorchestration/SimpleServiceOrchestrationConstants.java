b4833347e4ae7c4b649f31b61340cd9efa1d499e
Katinka666 <jako.katinka@gmail.com>
2025-04-23 17:53:36 +0200

    add general mgmt and monitor api

--- serviceorchestration-simple/src/main/java/eu/arrowhead/serviceorchestration/SimpleServiceOrchestrationConstants.java
+++ serviceorchestration-simple/src/main/java/eu/arrowhead/serviceorchestration/SimpleServiceOrchestrationConstants.java
@@ -7,8 +7,14 @@ public final class SimpleServiceOrchestrationConstants {
 
 	public static final String SYSTEM_NAME = "serviceorchestration-simple";
 	
+	// DB
 	public static final String DATABASE_ENTITY_PACKAGE = "eu.arrowhead.serviceorchestration.jpa.entity";
 	public static final String DATABASE_REPOSITORY_PACKAGE = "eu.arrowhead.serviceorchestration.jpa.repository";
+	
+	// HTTP API
+	public static final String HTTP_API_BASE_PATH = "/serviceorchestration";
+	public static final String HTTP_API_MONITOR_PATH = HTTP_API_BASE_PATH + "/monitor";
+	public static final String HTTP_API_GENERAL_MANAGEMENT_PATH = HTTP_API_BASE_PATH + "/general/mgmt";
 
 	//=================================================================================================
 	// assistant methods
