fa660be15e6c934e8a8ce89a0c4ed71c73d7d8f0
Katinka666 <jako.katinka@gmail.com>
2025-08-18 16:29:14 +0200

    system discovery service

--- serviceregistry/src/test/java/eu/arrowhead/serviceregistry/service/DeviceDiscoveryServiceTest.java
+++ serviceregistry/src/test/java/eu/arrowhead/serviceregistry/service/DeviceDiscoveryServiceTest.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
